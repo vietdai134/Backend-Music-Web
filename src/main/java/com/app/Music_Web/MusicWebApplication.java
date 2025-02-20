10f82d538800c0e8e3b8abe5aaf114759cfef85c
vietdai134 <vietnguyentran134@gmail.com>
2025-02-20 23:53:50 +0700

    update springboot

---
+++ src/main/java/com/app/Music_Web/MusicWebApplication.java
@@ -0,0 +1,13 @@
+package com.app.Music_Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicWebApplication.class, args);
+	}
+
+}
