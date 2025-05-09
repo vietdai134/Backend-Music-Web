1d500101358e361429f296bbc24042b26fb80e3a
vietdai134 <vietnguyentran134@gmail.com>
2025-05-09 09:39:07 +0700

    adjust setup cred.json

--- src/main/java/com/app/Music_Web/Infrastructure/Config/GoogleCredentialsSetup.java
+++ src/main/java/com/app/Music_Web/Infrastructure/Config/GoogleCredentialsSetup.java
@@ -1,31 +1,30 @@
 package com.app.Music_Web.Infrastructure.Config;
 
-import java.util.Base64;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Base64;
 
+@Component
 public class GoogleCredentialsSetup {
+    private static final String ENV_VARIABLE = "GOOGLE_CREDENTIALS_BASE64";
+    private static final String OUTPUT_FILE_PATH = "cred.json";
 
-    public static void setupCredentials() {
-        try {
-            // Lấy chuỗi base64 từ biến môi trường
-            String base64Credentials = System.getenv("GOOGLE_CREDENTIALS_BASE64");
+    @PostConstruct
+    public void init() throws IOException {
+        String base64 = System.getenv(ENV_VARIABLE);
 
-            if (base64Credentials == null || base64Credentials.isEmpty()) {
-                System.out.println("No base64 credentials found in environment variable!");
-                return;
-            }
-
-            // Giải mã chuỗi base64 thành byte[]
-            byte[] decodedBytes = Base64.getDecoder().decode(base64Credentials);
-
-            // Lưu nội dung vào file cred.json
-            Files.write(Paths.get("/app/cred.json"), decodedBytes);
-
-            System.out.println("Google credentials file saved successfully!");
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (base64 == null || base64.isEmpty()) {
+            throw new IllegalStateException("Environment variable " + ENV_VARIABLE + " is not set.");
         }
+
+        byte[] decodedBytes = Base64.getDecoder().decode(base64);
+        Files.write(Paths.get(OUTPUT_FILE_PATH), decodedBytes);
+        System.out.println("cred.json has been written successfully.");
     }
 }
 
