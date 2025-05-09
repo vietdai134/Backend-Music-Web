26aeae65418c861bfbcbd20c63937809f1cab6e0
vietdai134 <vietnguyentran134@gmail.com>
2025-05-09 08:51:52 +0700

    adjust setup cred.json

--- src/main/java/com/app/Music_Web/MusicWebApplication.java
+++ src/main/java/com/app/Music_Web/MusicWebApplication.java
@@ -2,10 +2,12 @@ package com.app.Music_Web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.app.Music_Web.Infrastructure.Persistence.Repositories")
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class MusicWebApplication {
 
